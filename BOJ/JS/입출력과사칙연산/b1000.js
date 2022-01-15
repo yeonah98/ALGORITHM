0de32d49aa4b9f468068f8dec3171a84e75bd349
yeonah98 <dbdusdkaa@naver.com>
2022-01-15 22:13:41 +0900

    b1000문제 코드 길이 줄이기 및 복습

--- BOJ/JS/입출력과사칙연산/b1000.js
+++ BOJ/JS/입출력과사칙연산/b1000.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString();
-input = input.split(' ');
-let arr = [];
-for (let i = 0; i < input.length; i++) {
-    arr.push(+input[i]);
-}
-solution(arr[0], arr[1]);
+
+const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(item => +item);
+
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// let input = fs.readFileSync(filePath).toString().split(' ').map(item => +item);
+
+// input = input.split(' ');
+// let arr = []; 
+// for (let i = 0; i < input.length; i++) {
+//     arr.push(+input[i]);
+// }
 
 function solution(A, B) {
     console.log(A + B);
 }
+
+solution(inputData[0], inputData[1]);
