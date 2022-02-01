231373b3e872b06b0a78bb4efeebeee14d2bd9b8
yeonah98 <dbdusdkaa@naver.com>
2022-02-01 12:50:43 +0900

    Feat: Add b2739.js

---
+++ BOJ/JS/for문/b2739_rl.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const solution = (n) => {
+    for(var i=1; i < 10; i++) {
+        result = i * n;
+        console.log(n + " * " + i + " = " + result);
+    }
+
+}
+
+rl.on('line', (line) => {
+    const input = line.split(' ').map(item => +item);
+    solution(input);
+}).on('close',() => {
+    process.exit();
+});
