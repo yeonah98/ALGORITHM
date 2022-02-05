e90575cd4e00fbedc9b6bdc14f70fba4a883b5dc
yeonah98 <dbdusdkaa@naver.com>
2022-02-05 19:44:25 +0900

    Feat: Add b11022.js

---
+++ BOJ/JS/for문/b11022_rl.js
@@ -0,0 +1,24 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let input = []
+
+rl.on('line', (line) => {
+    input.push(line)
+}).on('close', () => {
+    let number = +input[0]
+    let sum = 0
+    let answer = ''
+    for(let i = 1; i <= number; i++){
+        num1 = +input[i].split(' ')[0]
+        num2 = +input[i].split(' ')[1]
+        sum = num1 + num2
+
+        answer += 'Case #' + i + ': ' + num1 + ' + ' + num2 + ' = ' + sum + '\n'
+    }
+    console.log(answer)
+    process.exit()
+})
