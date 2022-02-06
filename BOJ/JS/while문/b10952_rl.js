ea32f6ce1d00046b50cf454b011f1dc714e9aca5
yeonah98 <dbdusdkaa@naver.com>
2022-02-06 19:27:16 +0900

    Feat: Add b10952.js

---
+++ BOJ/JS/while문/b10952_rl.js
@@ -0,0 +1,33 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let input = []
+
+rl.on('line', (line) => {
+    input.push(line)
+
+}).on('close', () => {
+    let sum = ''
+    input = input.map(x => x.split(' ').map(y => +y))
+
+    // let i = 0
+    // while(input[i][0] !== 0 && input[i][1] !== 0) {
+    //     sum += input[i][0] + input[i][1] + '\n'
+    //     i++
+    // }
+    // console.log(sum)
+    //process.exit()
+
+    let i = 0
+    while(true) {
+        if (input[i][0] == 0 && input[i][1] == 0) {
+            console.log(sum)
+            process.exit()
+        }
+        sum += input[i][0] + input[i][1] + '\n'
+        i++
+    }
+})
