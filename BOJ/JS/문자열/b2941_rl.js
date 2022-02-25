c43dbd75be253bebea2751b8dbf6f63122f85227
yeonah98 <dbdusdkaa@naver.com>
2022-02-25 20:18:27 +0900

    Feat: Add b2941.js

---
+++ BOJ/JS/문자열/b2941_rl.js
@@ -0,0 +1,16 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const solution = (n) => {
+
+}
+
+rl.on('line', (line) => {
+    input = line
+}).on('close', () => {
+  
+    process.exit()
+})
