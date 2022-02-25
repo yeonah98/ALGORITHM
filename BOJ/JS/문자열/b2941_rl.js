d6e5e30c7d1dad23063d62baf1c8e99aac5a9585
yeonah98 <dbdusdkaa@naver.com>
2022-02-25 20:56:41 +0900

    Feat: Update b2941.js

--- BOJ/JS/문자열/b2941_rl.js
+++ BOJ/JS/문자열/b2941_rl.js
@@ -5,12 +5,23 @@ const rl = readline.createInterface({
 })
 
 const solution = (n) => {
-
+    let cnt = 0
+    for(i in n) {
+        if(((n[i-1] == 'c' || n[i-1] == 's' || n[i-1] == 'z') && n[i] == '=') || ((n[i-1] == 'c' || n[i-1] == 'd') && n[i] == '-') || ((n[i-1] == 'l' || n[i-1] == 'n') && n[i] == 'j')) {
+            cnt -= 1
+            if(n[i-2] == 'd' && n[i-1] == 'z' && n[i] == '=') {
+                cnt -= 1
+            }
+        }
+        cnt++
+    }
+    console.log(cnt)
 }
 
 rl.on('line', (line) => {
     input = line
+
 }).on('close', () => {
-  
+    solution(input)
     process.exit()
 })
