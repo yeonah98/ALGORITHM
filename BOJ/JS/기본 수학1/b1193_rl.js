d943265687f11d178f37f38b636e29d14f75768c
yeonah98 <dbdusdkaa@naver.com>
2022-03-19 23:35:42 +0900

    Feat :b1193.js 정답코드 추가

--- BOJ/JS/기본 수학1/b1193_rl.js
+++ BOJ/JS/기본 수학1/b1193_rl.js
@@ -4,6 +4,25 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+
+
+/*
+let value = 1;
+while (true) {
+	number -= value;
+	if (number <= 0) {
+		number += value;
+		break;
+	}
+	value++;
+}
+
+if (value % 2 === 1) {
+	console.log(`${value - (number - 1)}/${1 + (number - 1)}`);
+} else {
+	console.log(`${1 + (number - 1)}/${value - (number - 1)}`);
+}
+*/
 const A = (n) => {
     let A = []
     let cnt = 1
