ae08777d79b7f841726f4ca5f635479d8dc6b23c
yeonah98 <dbdusdkaa@naver.com>
2022-03-26 17:05:50 +0900

    Feat: Solved b2775_rl.js

--- BOJ/JS/기본 수학1/b2775_rl.js
+++ BOJ/JS/기본 수학1/b2775_rl.js
@@ -1,17 +1,33 @@
-const readline = require('readline')
+const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
-const solution = (n) => {
-  
+const solution = (k, n) => {
+    let house = Array.from(Array(k+1), () => Array(n+1).fill(0));
+
+    for(let i=0; i<=n; i++) {
+        house[0][i] = i;
+    }
+
+    for(let i=1; i<=k; i++) {
+        for(let j=1; j<=n; j++) {
+            house[i][j] = house[i-1][j] + house[i][j-1]
+        }
+    }
+
+    console.log(house[k][n]);
 }
 
+input = [];
 rl.on('line', (line) => {
-    input = +line
+    input.push(+line);
 
 }).on('close', () => {
-  
-    process.exit()
-})
+    for(let i=1; i<input[0]*2; i+=2) {
+        solution(input[i], input[i+1]);
+    }
+
+    process.exit();
+});
